7d9945b258e476d4d2be9110f15330657bb09413
Ross Beyer <rbeyer@rossbeyer.net>
2019-02-12 21:28:00 -0800

    I know, there's an import * here, but the plan is to make the kalasiris.py file independent, and there are a large number of potential names to import in there, so * it is.

--- kalasiris/__init__.py
+++ kalasiris/__init__.py
@@ -5,3 +5,7 @@
 __author__ = """Ross A. Beyer"""
 __email__ = 'rbeyer@seti.org'
 __version__ = '0.0.0'
+
+from .kalasiris import *
+from .Histogram import Histogram
+from .cubenormDialect import cubenormDialect
