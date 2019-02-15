2e779288a0cdb63028f03ced923ea52eceb0846b
Ross Beyer <rbeyer@rossbeyer.net>
2019-02-14 17:00:51 -0800

    flake8ed

--- kalasiris/__init__.py
+++ kalasiris/__init__.py
@@ -6,6 +6,6 @@ __author__ = """Ross A. Beyer"""
 __email__ = 'rbeyer@seti.org'
 __version__ = '0.0.0'
 
-from .kalasiris import *
-from .Histogram import Histogram
-from .cubenormDialect import cubenormDialect
+from .kalasiris import *                      # noqa: F401,F403
+from .Histogram import Histogram              # noqa: F401
+from .cubenormDialect import cubenormDialect  # noqa: F401
