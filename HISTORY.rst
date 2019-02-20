a5a908cb486915fbecf6175f2359ff993b70e8e0
Ross Beyer <rbeyer@rossbeyer.net>
2019-02-19 16:57:10 -0800

    Tightened up the implementation, and polished the documentation.

--- HISTORY.rst
+++ HISTORY.rst
@@ -6,3 +6,17 @@ History
 ------------------
 
 * Started project.
+
+
+Road to 0.1.0
+-------------
+- understand bump2version
+
+
+Future History
+--------------
+*Things that will become history (i.e. To Do items).*
+
+- think about applying functools.update_wrapper and functools.wraps
+  in ``kalasiris.py``
+  https://docs.python.org/3.6/library/functools.html#module-functools
--- kalasiris/Histogram.py
+++ kalasiris/Histogram.py
@@ -19,6 +19,7 @@ import collections
 import csv
 import itertools
 import os
+import subprocess
 from .k_funcs import hist_k
 
 
@@ -38,29 +39,16 @@ class Histogram(collections.abc.Sequence):
     def __init__(self, histinfo):
         self.histinfo = histinfo
 
-        # if isinstance(histinfo, os.PathLike):
-        if os.path.isfile(histinfo):
-            with open(histinfo) as f:
-                iscube = f.readline().split(':')[0]
-
-            if 'Cube' == iscube:
-                with open(histinfo, 'r') as f:
-                    contents = f.read()
-
-                (self.dictionary, self.headers, self.hist_list) \
-                    = self.parse(contents)
-            else:
+        try:
+            (self.dictionary, self.headers, self.hist_list) = self.parse(histinfo)
+        except StopIteration:
+            try:
                 (self.dictionary, self.headers, self.hist_list) \
                     = self.parse(hist_k(histinfo))
-
-            # (self.dictionary, self.headers, self.hist_list) \
-            #     = self.parsehistfile(histinfo)
-        else:
-            (self.dictionary, self.headers, self.hist_list) \
-                = self.parse(histinfo)
-
-        # self.hist_list is a list of
-        # collections.namedtuple( 'HistRow', self.headers )
+            except subprocess.CalledProcessError:
+                with open(histinfo, 'r') as f:
+                    (self.dictionary, self.headers, self.hist_list) \
+                        = self.parse(f.read())
 
     def __str__(self):
         return(str(self.dictionary))
@@ -72,15 +60,10 @@ class Histogram(collections.abc.Sequence):
         return len(self.hist_list)
 
     def __getitem__(self, key):
-        # if key is integer or slice object, look in self.hist_list
-        if isinstance(key, int):
-            return self.hist_list[key]
-
-        elif isinstance(key, slice):
-            return self.hist_list[key]
-
-        elif isinstance(key, str):
+        try:
             return self.dictionary[key]
+        except KeyError:
+            return self.hist_list[key]
 
     def __iter__(self):
         return self.hist_list.__iter__()
@@ -88,10 +71,8 @@ class Histogram(collections.abc.Sequence):
     def __contains__(self, item):
         if item in self.dictionary:
             return True
-        elif item in self.hist_list:
-            return True
         else:
-            return False
+            return(item in self.hist_list)
 
     def keys(self):
         '''Gets the keys from the initial portion of the hist output file.
@@ -118,55 +99,54 @@ class Histogram(collections.abc.Sequence):
            The contents of the file that results from ISIS ``hist`` look like
            this::
 
-            Cube:           PSP_010502_2090_RED5_0.EDR_Stats.cub
-            Band:           1
-            Average:        6490.68
-            [... other lines like this ...]
-            His Pixels:      0
-            Hrs Pixels:      0
+                Cube:           PSP_010502_2090_RED5_0.EDR_Stats.cub
+                Band:           1
+                Average:        6490.68
+                [... other lines like this ...]
+                His Pixels:      0
+                Hrs Pixels:      0
 
 
-            DN,Pixels,CumulativePixels,Percent,CumulativePercent
-            3889,1,1,4.88281e-05,4.88281e-05
-            3924,1,2,4.88281e-05,9.76563e-05
-            3960,2,4,9.76563e-05,0.000195313
-            [... more comma-separated lines like above ...]
+                DN,Pixels,CumulativePixels,Percent,CumulativePercent
+                3889,1,1,4.88281e-05,4.88281e-05
+                3924,1,2,4.88281e-05,9.76563e-05
+                3960,2,4,9.76563e-05,0.000195313
+                [... more comma-separated lines like above ...]
 
            But this function sees it like this::
 
-            k: v
-            k: v
-            k: v
-            [... other lines like this ...]
-            k: v
-            k: v
+                k: v
+                k: v
+                k: v
+                [... other lines like this ...]
+                k: v
+                k: v
 
 
-            h,h,h,h,h
-            n,n,n,n,n
-            n,n,n,n,n
-            n,n,n,n,n
-            [... more comma-separated lines like above ...]
+                h,h,h,h,h
+                n,n,n,n,n
+                n,n,n,n,n
+                n,n,n,n,n
+                [... more comma-separated lines like above ...]
 
-           First, it takes all of the ``k``s and ``v``s and saves them as the
-           keys and values in the returned Histogram's dictionary.
+           Where each of the letters above is a string value that the parser
+           reads.
 
-           It takes the ``h``s and uses them as the set of fieldnames in the
-           Histogram.
+           First, it takes all of the ``k`` and ``v`` elements and saves them as the
+           keys and values in the returned dictionary.
 
-           It takes all of the lines with ``n``s and stores them as
-           ``namedtuples`` in the Histogram's primary list.
+           Second, the ``h`` elements are returned them as the list of fieldnames.
 
+           Third, it reads the lines with ``n`` and stores them as
+           ``namedtuples`` in the returned list.
         '''
         d = dict()
         fieldnames = []
         hist_rows = []
         for line in filter(lambda x: ':' in x, histinfo.splitlines()):
             (k, v) = line.split(':')
-            if 'Cube' in k:
-                d[k.strip()] = v.strip()
-            else:
-                d[k.strip()] = float(v.strip())
+            # d[k.strip()] = v.strip()
+            d.setdefault(k.strip(), v.strip())
 
         reader = csv.reader(filter(lambda x: ',' in x, histinfo.splitlines()))
         fieldnames = next(reader)
@@ -175,7 +155,6 @@ class Histogram(collections.abc.Sequence):
         for row in map(HistRow._make, reader):
             hist_rows.append(row)
 
-        # Why not just return a Histogram here?:
-        HistParsed = collections.namedtuple('HistParsed', ['info',
-                                            'fieldnames', 'data'])
+        HistParsed = collections.namedtuple('HistParsed',
+                                            ['info', 'fieldnames', 'data'])
         return(HistParsed(d, fieldnames, hist_rows))
