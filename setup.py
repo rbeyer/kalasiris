27840adf6b8c17e4eb07a5375fdcffd50d7f0e5a
Ross Beyer <rbeyer@rossbeyer.net>
2019-10-06 18:13:10 -0700

    Bump version: 1.2.0 → 1.3.0-dev

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rbeyer/kalasiris',
-    version='1.2.0',
+    version='1.3.0-dev',
     zip_safe=False,
 )
