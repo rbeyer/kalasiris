9891bb3b4a55dda46d627d1d6ecb82e3d5dbd2e9
Ross Beyer <rbeyer@rossbeyer.net>
2024-07-10 18:06:00 -0700

    Bump version: 1.11.0-dev → 1.11.0

--- kalasiris/kalasiris.py
+++ kalasiris/kalasiris.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # This file shall have *NO* non-Standard Library dependencies.
 
 # kalasiris library version:
-__version__ = "1.11.0-dev"
+__version__ = "1.11.0"
 
 # Set a logger:
 logger = logging.getLogger(__name__)
